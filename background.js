06f655507ee5dcdf694913c61441302266a36b9d
Muskaan Aggarwal <muskaanaggarwal273@gmail.com>
2026-02-25 20:31:10 +0530

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-// logic hub
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
-
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.type === "SCAN") {
-    chrome.storage.local.set({ lead: msg.data });
-    chrome.runtime.sendMessage({ type: "UI_UPDATE", data: msg.data });
-  }
-});
